f1c9856467698aa25a2f2d9a574f8a75d184dab6
87-MaviaSaleem <maviasaleem371@gmail.com>
2022-04-21 23:58:24 +0500

    Image added

---
+++ webproj/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="header-container d-flex justify-content-center align-items-center">
+      <div>
+        <img
+          className="header-image"
+          src="https://scontent.flhe3-2.fna.fbcdn.net/v/t1.6435-9/192594659_1237262833399577_8896790266484511464_n.jpg?stp=dst-jpg_s960x960&_nc_cat=110&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeGb275gs-bAABg9ZUal9epvZL4FBlGa_CdkvgUGUZr8J71nGKVS81t0IJvEXXe7DdKhSBhFudwphYHoOGFduKoC&_nc_ohc=LWwtvbUBoloAX8klJKJ&_nc_ht=scontent.flhe3-2.fna&oh=00_AT81udUE0lBdThM5SrtKov3ps7IX_oaXrprf1joIwknebQ&oe=628880C3"
+        ></img>
+      </div>
+    </div>
+  );
+};
